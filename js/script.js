c8bfe04300ef9ddef93dd3372bdb8db90bd2e21d
Maxime_Cottin <github@maxime-cottin.fr>
2021-08-18 15:40:15 +0200

    Enfin le pokémon s'affiche!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var img = new Image();
-img.src = '../res/063-abra.png';
+img.src = '../res/033-nidorino.png';
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
@@ -14,24 +14,28 @@ for (var i = 0; i < 112; i++)
 function disp_img() {
   for (let i = 0; i < tableau.length; i++) {
     for (let j = 0; j < tableau[i].length; j++) {
-
       var pixelImg = ctx.getImageData(j, i, 1, 1);
       var data = pixelImg.data;
       var red = data[0];
       var green = data[1];
       var blue = data[2];
       var alpha = data[3]/255;
+      console.log("X= " + j);
+      console.log("Y= " + i);
       console.log(red, green, blue, alpha);
-      $( "section" ).append( "<div class=\"pixel\"></div>");
-      $('.pixel').last().css("background-color" , `rgba (${red},${green},${blue},${alpha})`);
-      console.log("x = " + (j+1));
-      console.log("y = " + (i+1));
-
+      $( "section" ).append( "<div class=\"pixel\" style=\"background-color:rgba(" + red + "," + green + "," + blue + "," + alpha + ")\"></div>");
     }
   }
-};     
+};   
+function colorTab() {
+  for (let i = 0; i<tableau.length; i++) {
+    for (let j = 0; j<tableau[i].length; j++) {
+    }
+  }
+}  
 
 img.onload = function() {
   ctx.drawImage(img, 0, 0);
   disp_img();
+  colorTab();
 };  
